c1f1a0e7ab688adffce8f0868818b716d2554aa4
Cancún <fornara.federico@gmail.com>
2026-05-29 20:09:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -124,13 +124,6 @@ function openModal() {
     `https://wa.me/529981308940?text=${encodeURIComponent(message)}`;
 }
 
-  const message =
-    `Hola, me interesa ${currentProduct.Nome}`;
-
-  document.getElementById("productWhatsapp").href =
-    `https://wa.me/529981308940?text=${encodeURIComponent(message)}`;
-
-
 function closeModal() {
   modal.style.display = "none";
 }
